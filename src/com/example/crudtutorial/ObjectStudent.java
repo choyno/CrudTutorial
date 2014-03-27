65ed12b38512b45bd9617a689f24a97e9e427f6c
choyno <ariel.lopezf@gmail.com>
2014-03-27 12:30:14 +0800

    crud working

---
+++ src/com/example/crudtutorial/ObjectStudent.java
@@ -0,0 +1,14 @@
+package com.example.crudtutorial;
+
+public class ObjectStudent { 
+	
+    int id;
+    String firstname;
+    String email;
+    
+    public ObjectStudent(){ 
+        
+    }
+    
+    
+}
